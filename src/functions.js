9d0c2eabd5da0986e87f58419a605ed2585cc168
dsgnfox <dsgnfox@gmail.com>
2021-10-19 22:00:09 +0300

    add brain gcd game

--- src/functions.js
+++ src/functions.js
@@ -1,2 +1,10 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 export const isOddNumber = (number) => number % 2 === 0;
+
+export const getGcd = (a, b) => {
+  if (b) {
+    return getGcd(b, a % b);
+  }
+  return Math.abs(a);
+};
