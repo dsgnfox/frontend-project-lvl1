70d3f52f4e39e14ff425e232aa56017d2cc1ea24
dsgnfox <dsgnfox@gmail.com>
2021-11-01 20:22:30 +0300

    eslint fix

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,4 +1,6 @@
 import { MAX_RANDOM_NUMBER, MIN_RANDOM_NUMBER } from './constants.js';
 
+/* eslint-disable-next-line */
 const getRandomNumber = (min = MIN_RANDOM_NUMBER, max = MAX_RANDOM_NUMBER) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 export default getRandomNumber;
